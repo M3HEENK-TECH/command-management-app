5656ca35b74b94c0e6c057b30ef59ed4f29de8f8
StevyMarlino <stevyabessolo@gmail.com>
2020-04-19 21:40:00 +0200

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,14 +21,9 @@ $factory->define(User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-<<<<<<< Updated upstream
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
-=======
         'password' => bcrypt("password"),
         'profile_image' => 'D:\Medias\Mes Logos\2.jpg',
         'remember_token' => Str::random(10),
         "role" => "admin"
->>>>>>> Stashed changes
     ];
 });
