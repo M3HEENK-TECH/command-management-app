da0e3706b530fe7a4387fb9f67ab05d0e57f6fc2
StevyMarlino <stevyabessolo@gmail.com>
2020-05-12 07:27:29 +0200

    Notification
    affiche le seuil de stock en dessa de 10 ou egal à 10

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -1,12 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Product;
 
 use Illuminate\Http\Request;
 
 class NotificationController extends Controller
 {
     public function list(){
+
+       $notiproduct = Product::select('name','quantity')->where('quantity','<=','10')->get();
+
+       return view('resources.notification.index',compact('notiproduct'));
 
     }
 }
