755a41e18e10178e79fbdc8d7b00340ef355eab8
StevyMarlino <stevyabessolo@gmail.com>
2020-05-20 20:03:39 +0200

    Update SalesController.php
    on retire le dd

--- app/Http/Controllers/Resources/SalesController.php
+++ app/Http/Controllers/Resources/SalesController.php
@@ -65,9 +65,7 @@ class SalesController extends Controller
      */
     public function store(StoreSalesRequest $request)
     {
-        dd(
-            $request->all()
-        );
+  
         $product = Product::query()
             ->where("id", $request->get("product_id"))
             ->where("quantity", ">=", $request->get("quantity") )
